563a2535774374301b8f58a660c2ba1fe81f7a3a
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-02-15 14:32:10 +0200

    Updated Auth (Add Email Validation )

--- Crowd_Funding_App/Crowd_Funding_App/urls.py
+++ Crowd_Funding_App/Crowd_Funding_App/urls.py
@@ -24,10 +24,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("admin/", admin.site.urls),
     path("", include("home_app.urls")),
     path("user/", include("user_auth_app.urls")),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
